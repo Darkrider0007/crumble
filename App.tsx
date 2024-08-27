cf8abe900460073d014713dcf080184b95fda508
Rohan Gope <rg2512@it.jgec.ac.in>
2024-08-27 19:58:52 +0530

    chore: Add SVG module declaration and update store configuration

--- App.tsx
+++ App.tsx
@@ -1,118 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
+import store from './src/store/store';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { enableScreens } from 'react-native-screens';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Enable react-native-screens for performance optimization
+enableScreens();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+// Import your screens
+import LoaderScreen from './src/screens/auth/Loading';
+import Signin from './src/screens/auth/Signin';
+import Home from './src/screens/landingpage/Home';
+import Signup from './src/screens/auth/Signup';
+import OTPVerify from './src/screens/auth/OTPVerify';
+import Location from './src/screens/auth/Location';
+import SigninSuccess from './src/screens/auth/SigninSuccess';
+import Search from './src/screens/landingpage/Search';
+import SearchResult from './src/screens/landingpage/SearchResult';
+import Details from './src/screens/landingpage/Details';
+import CartScreen from './src/screens/landingpage/CartScreen';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="LoaderScreen">
+          <Stack.Screen name="LoaderScreen" component={LoaderScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Signin" component={Signin} options={{ headerShown: false, title: 'Sign In' }} />
+          <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false, title: 'Sign Up' }} />
+          <Stack.Screen name="OTPVerify" component={OTPVerify} options={{ headerShown: false, title: 'Verify OTP' }} />
+          <Stack.Screen name="Location" component={Location} options={{ headerShown: false, title: 'Location' }} />
+          <Stack.Screen name="SigninSuccess" component={SigninSuccess} options={{ headerShown: false, title: 'Sign In Success' }} />
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false, title: 'Home' }} />
+          <Stack.Screen name="Search" component={Search} options={{ title: 'Search' }} />
+          <Stack.Screen name="SearchResult" component={SearchResult} options={{ title: 'Search Result' }} />
+          <Stack.Screen name="Details" component={Details} options={{ title: 'Details' }} />
+          <Stack.Screen name="CartScreen" component={CartScreen} options={{ headerShown: false, title: 'Cart' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
